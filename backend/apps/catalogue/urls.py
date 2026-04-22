81aaf1b09d3b44d799e51ba2a519a9b280225d07
HarivolaANDY <hei.andy.43@gmail.com>
2026-04-23 00:16:56 +0300

    feat: implement comprehensive user management module and system UI framework

--- backend/apps/catalogue/urls.py
+++ backend/apps/catalogue/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, SupplierViewSet, ProductViewSet, ProduitDvViewSet
+from .views import CategoryViewSet, SupplierViewSet, ProductViewSet, ProduitDvViewSet, RevenueViewSet
 
 router = DefaultRouter()
 router.register(r'categories',  CategoryViewSet,  basename='category')
 router.register(r'suppliers',   SupplierViewSet,  basename='supplier')
 router.register(r'products',    ProductViewSet,   basename='product')
 router.register(r'produits-dv', ProduitDvViewSet, basename='produit-dv')
+router.register(r'revenues',    RevenueViewSet,   basename='revenue')
 #revenu mensuel??
 
 urlpatterns = [
